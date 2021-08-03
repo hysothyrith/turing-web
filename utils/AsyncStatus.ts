48caf2bbf5d6a793331111977cba324d785477b0
Sothyrith Hy <sothyrithhy@gmail.com>
2021-08-03 22:10:35 +0700

    feat: add AsyncStatus

---
+++ utils/AsyncStatus.ts
@@ -0,0 +1,37 @@
+export default class AsyncStatus {
+  status = 'idle'
+  timeout: NodeJS.Timeout = setTimeout(() => {}, 0)
+
+  isIdle() {
+    return this.status === 'idle'
+  }
+
+  isLoading() {
+    return this.status === 'loading'
+  }
+
+  isResolved() {
+    return this.status === 'resolved'
+  }
+
+  isRejected() {
+    return this.status === 'rejected'
+  }
+
+  beginLoading(delay = 200) {
+    clearTimeout(this.timeout)
+    this.timeout = setTimeout(() => {
+      this.status = 'loading'
+    }, delay)
+  }
+
+  resolve() {
+    clearTimeout(this.timeout)
+    this.status = 'resolved'
+  }
+
+  reject() {
+    clearTimeout(this.timeout)
+    this.status = 'rejected'
+  }
+}
