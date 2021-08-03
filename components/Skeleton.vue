88b3b994e9a6655ba991a85d6ae6060efc7bd460
Sothyrith Hy <sothyrithhy@gmail.com>
2021-08-03 22:11:12 +0700

    feat: add Skeleton component

---
+++ components/Skeleton.vue
@@ -0,0 +1,44 @@
+<template>
+  <div
+    class="skeleton"
+    :style="{ width, height, animationDelay: `${delay * 0.1}s` }"
+  />
+</template>
+
+<script>
+export default {
+  props: {
+    delay: {
+      type: Number,
+      default: 0,
+    },
+    width: {
+      type: String,
+      default: '100%',
+    },
+    height: {
+      type: String,
+      default: '100%',
+    },
+  },
+}
+</script>
+
+<style scoped>
+.skeleton {
+  background-color: grey;
+  animation: pulse 2s infinite;
+}
+
+@keyframes pulse {
+  0% {
+    background-color: grey;
+  }
+  70% {
+    background-color: #454545;
+  }
+  100% {
+    background-color: grey;
+  }
+}
+</style>
