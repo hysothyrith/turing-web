2223677c333b1b4b2967c7c34142133ca6475118
Sothyrith Hy <sothyrithhy@gmail.com>
2021-08-15 11:48:56 +0700

    feat: redirect if unauthenticated on protected routes

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,17 +1,20 @@
 import { Mutations } from '~/constants'
 
-export default function ({ store }) {
+export default function ({ store, route, redirect }) {
   if (process.server) {
     return
   }
 
-  function setAuthData() {
-    const user = localStorage.getItem('user')
-    const token = localStorage.getItem('token')
-    if (user && token) {
-      store.commit(Mutations.SET_AUTH_DATA, { user: JSON.parse(user), token })
+  const user = localStorage.getItem('user')
+  const token = localStorage.getItem('token')
+  if (user && token) {
+    store.commit(Mutations.SET_AUTH_DATA, { user: JSON.parse(user), token })
+  } else {
+    const isProtected = route.meta.some(
+      (meta) => meta.auth && meta.auth.protected
+    )
+    if (isProtected) {
+      return redirect('/')
     }
   }
-
-  setAuthData()
 }
