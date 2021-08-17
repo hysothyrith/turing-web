a9a6fcca0f9b3c988d320ef70eb2d0e3322e44a3
Sothyrith Hy <sothyrithhy@gmail.com>
2021-08-17 07:49:12 +0700

    perf: register only needed PhosphorVue icons

--- plugins/phosphor-vue.js
+++ plugins/phosphor-vue.js
@@ -1,4 +1,27 @@
 import Vue from 'vue'
-import PhosphorVue from 'phosphor-vue'
+// import PhosphorVue from 'phosphor-vue'
 
-Vue.use(PhosphorVue)
+// Vue.use(PhosphorVue)
+
+import {
+  PhPlay,
+  PhTicket,
+  PhCaretRight,
+  PhCaretDown,
+  PhCheck,
+} from 'phosphor-vue'
+
+const icons = Object.assign(
+  {},
+  {
+    PhPlay,
+    PhTicket,
+    PhCaretRight,
+    PhCaretDown,
+    PhCheck,
+  }
+)
+
+Object.entries(icons).forEach(([iconName, icon]) => {
+  Vue.component(iconName, icon)
+})
