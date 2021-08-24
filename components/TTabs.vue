9dbcc8304a73088c1769c0923ec8e0e7e93af3f4
Sothyrith Hy <sothyrithhy@gmail.com>
2021-08-24 10:50:58 +0700

    feat: add tabs component

--- components/TTabs.vue
+++ components/TTabs.vue
@@ -1,7 +1,16 @@
 <template>
-  <div class="tabs">
-    <div>Tab 1</div>
-    <div>Tab 2</div>
+  <div class="tabs HIDE-DEBUG">
+    <ul class="tabs__header DEBUG-R">
+      <li v-for="(tab, i) in tabs" :key="tab.title">
+        <button
+          :class="['tab__header DEBUG-G', { selected: i === selectedIndex }]"
+          @click="selectTab(i)"
+        >
+          {{ tab.title }}
+        </button>
+      </li>
+    </ul>
+    <slot />
   </div>
 </template>
 
@@ -9,8 +18,47 @@
 export default {
   data() {
     return {
-      index: 0,
+      selectedIndex: 0,
+      tabs: [],
     }
+  },
+  created() {
+    this.tabs = this.$children
+  },
+  mounted() {
+    this.selectedIndex = this.$children.findIndex((el) => el.selected)
+  },
+  methods: {
+    selectTab(i) {
+      this.selectedIndex = i
+      this.tabs.forEach((tab, tabIndex) => {
+        tab.isActive = tabIndex === i
+      })
+    },
   },
 }
 </script>
+
+<style scoped>
+.tabs__header {
+  list-style: none;
+  display: flex;
+  padding-left: 0;
+  border-bottom: 1px solid var(--color-muted);
+  padding-bottom: var(--spacing-2);
+}
+
+.tab__header {
+  font-size: var(--size-h5);
+  color: var(--color-primary);
+  background-color: transparent;
+  /* border: none; */
+  /* outline: none; */
+  padding-left: 0;
+  margin-right: var(--spacing-6);
+}
+
+.tab__header.selected {
+  color: var(--color-prominent);
+}
+</style>
